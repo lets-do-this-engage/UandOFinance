f972a966478210a476583c388537e31af3ce6bcc
Corey Italiano <coreyitaliano@gmail.com>
2020-06-27 01:04:20 -0400

    Added a main page that lists fake data. Incorporated Jinja2 templates.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Corey'}
+    positions = [
+        {
+            'owner': {'username': 'Corey'},
+            'symbol': 'AAPL'
+        },
+        {
+            'owner': {'username': 'Justin'},
+            'symbol': 'MO'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, positions=positions)
