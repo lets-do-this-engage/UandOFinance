aeb69cf4d4374fdeae26ec432eb4c5f5e92aa90b
Corey Italiano <coreyitaliano@gmail.com>
2020-06-27 15:07:14 -0400

    Decided to stay more in line with the microblog tutorial just for simplicity sake in following the program. Once I finish the tutorial I will make this my own.

--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,25 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Corey'}
-    positions = [
+    user = {'username': 'Miguel'}
+    posts = [
         {
-            'owner': {'username': 'Corey'},
-            'symbol': 'AAPL'
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
         },
         {
-            'owner': {'username': 'Justin'},
-            'symbol': 'MO'
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, positions=positions)
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -24,4 +27,4 @@ def login():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('login.html',  title='Sign In', form=form)
