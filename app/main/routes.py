d5e4136461fe3e795a7d189fe9903bc1fb569ab9
Corey Italiano <coreyitaliano@gmail.com>
2020-08-14 00:08:05 -0400

    Added link for portfolio

--- app/main/routes.py
+++ app/main/routes.py
@@ -99,6 +99,32 @@ def accounts():
     return render_template('index.html', title=_('Home'), form=form,
                            posts=posts.items, next_url=next_url, accounts=accounts,
                            prev_url=prev_url)
+						   
+@bp.route('/portfolio', methods=['GET', 'POST'])
+@login_required
+def portfolio():
+    form = PostForm()
+    if form.validate_on_submit():
+        language = guess_language(form.post.data)
+        if language == 'UNKNOWN' or len(language) > 5:
+            language = ''
+        post = Post(body=form.post.data, author=current_user,
+                    language=language)
+        db.session.add(post)
+        db.session.commit()
+        flash(_('Your post is now live!'))
+        return redirect(url_for('main.index'))
+    accounts = [{'name':'Bank 1', 'balance':'2000'}]
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(
+        page, current_app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('main.index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('main.index', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title=_('Home'), form=form,
+                           posts=posts.items, next_url=next_url, accounts=accounts,
+                           prev_url=prev_url)
 
 
 @bp.route('/explore')
